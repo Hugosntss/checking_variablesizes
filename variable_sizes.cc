63fb4d1deddacc41991b2054de2a4dae7cf98e1c
Hugo <hugos@Hugos-MacBook-Air.local>
2024-09-15 17:47:13 -0700

    checking variables sizes

---
+++ variable_sizes.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+cout << "Computing the size of some c++ variables"<<endl;
+
+cout << "Size of Short: "<< sizeof(short)<<" bytes"<<endl;
+cout << "Size of Double: "<< sizeof(double)<<" bytes"<<endl;
+
+
+
+}
