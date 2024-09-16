f9f42c19997ff4c06438865cec4d359b7161f064
Hugo <hugos@Hugos-MacBook-Air.local>
2024-09-15 17:48:50 -0700

    comment sizeof

--- variable_sizes.cc
+++ variable_sizes.cc
@@ -5,7 +5,7 @@ int main(){
 
 cout << "Computing the size of some c++ variables"<<endl;
 
-cout << "Size of Short: "<< sizeof(short)<<" bytes"<<endl;
+cout << "Size of Short: "<< sizeof(short)<<" bytes"<<endl;   // sizeof is a variable to check the size of the variable
 cout << "Size of Double: "<< sizeof(double)<<" bytes"<<endl;
 
 
